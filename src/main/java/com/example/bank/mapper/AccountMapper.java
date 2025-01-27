cf1578472f6b0541c2e02e1577325cef041b4967
eglereact <eglereactjs@gmail.com>
2025-01-27 11:51:50 +0200

    first commit

---
+++ src/main/java/com/example/bank/mapper/AccountMapper.java
@@ -0,0 +1,17 @@
+package com.example.bank.mapper;
+
+import com.example.bank.dto.AccountDto;
+import com.example.bank.entity.Account;
+
+public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(accountDto.getId(), accountDto.getAccountHolderName(), accountDto.getBalance());
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(account.getId(), account.getAccountHolderName(),account.getBalance());
+        return accountDto;
+    }
+}
---
+++ src/main/java/com/example/bank/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.example.bank.service;
+
+import com.example.bank.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id, double amount);
+    AccountDto withdraw(Long id, double amount);
+    List<AccountDto> getAllAccounts();
+    void deleteAccount(Long id);
+}
